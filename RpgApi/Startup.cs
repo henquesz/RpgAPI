31a98dd1e4ad3f4b149cfb8566b5d30b8d367a53
henquesz <henquesvinicius@gmail.com>
2022-08-04 08:57:53 -0300

    first commit

--- RpgApi/Startup.cs
+++ RpgApi/Startup.cs
@@ -33,7 +33,7 @@ namespace RpgApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("ConexaoLocal")));
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("ConexaoSomee")));
 
             services.AddControllers();
 
